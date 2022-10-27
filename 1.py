4d2d7f074b0b17fedc77dfb8dbd18b7960f3b27c
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-27 23:05:34 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,5 @@
-#!/usr/bin/env python3
-import minimalmodbus
-
-instrument = minimalmodbus.Instrument('dev/ttyUSB0', 1)  # port name, slave address (in decimal)
-
-## Read temperature (PV = ProcessValue) ##
-temperature = instrument.read_register(289, 1)  # Registernumber, number of decimals
-print(temperature)
-
-## Change temperature setpoint (SP) ##
-NEW_TEMPERATURE = 95
-instrument.write_register(24, NEW_TEMPERATURE, 1)  # Registernumber, value, number of decimals for storage
+import serial
+ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+print(ser.name)         # check which port was really used
+ser.write(b'hello')     # write a string
+ser.close()             # close port
