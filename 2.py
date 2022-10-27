93c318d6d4de56b21a728a2c4cee85178f21571b
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-28 00:21:17 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,16 +6,16 @@ with serial.Serial(port='/dev/ttyUSB0', baudrate=9600, bytesize=serial.EIGHTBITS
 	
 	print(ser.name)         # check which port was really used
 	x = ser.read()          # read one byte
-	s = ser.read(2)        # read up to ten bytes (timeout)
+	s = ser.read(20)        # read up to ten bytes (timeout)
 	line = ser.readline(100)   # read a '\n' terminated line (timeout)
 	#lines = ser.readlines(5)   # read multiple '\n' terminated lines (timeout)
 	
 	print(x)
 	f.write(str(x)+"\n")
 	print(s)
-	f.write(str(s))
+	f.write(str(s)+"\n")
 	print(line)
-	f.write(str(line))
+	f.write(str(line)+"\n")
 	#print(lines)
 	#f.write(str(lines))
 	
