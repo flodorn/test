d6334658ee11a340bfd49a813d903bfd56719160
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-27 23:53:51 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,7 +4,7 @@ with serial.Serial('/dev/ttyUSB0', 9600, timeout=1) as ser:
 	print(ser.name)         # check which port was really used
 	x = ser.read()          # read one byte
 	s = ser.read(5)        # read up to ten bytes (timeout)
-	line = ser.readline()   # read a '\n' terminated line
+	line = ser.readline(5)   # read a '\n' terminated line (timneout)
 	lines = ser.readlines(5)   # read multiple '\n' terminated lines (timeout)
 	f = open("log.txt", "a")
 	print(x)
