b05da33d8f43e110adf54e9d79e9f58a7e90e975
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-27 22:01:00 +0200

    Update bus.py

--- bus.py
+++ bus.py
@@ -1,1 +1,7 @@
-#test
+from pymodbus.client import ModbusTcpClient
+
+client = ModbusTcpClient('127.0.0.1')
+client.write_coil(1, True)
+result = client.read_coils(1,1)
+print(result.bits[0])
+client.close()
