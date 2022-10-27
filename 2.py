6fffb6fcfdeeec34933c04bb22e849b3512ec855
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-28 00:01:07 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -7,7 +7,7 @@ with serial.Serial('/dev/ttyUSB0', 9600, timeout=1) as ser:
 	x = ser.read()          # read one byte
 	s = ser.read(5)        # read up to ten bytes (timeout)
 	line = ser.readline(5)   # read a '\n' terminated line (timneout)
-	lines = ser.readlines(5)   # read multiple '\n' terminated lines (timeout)
+	#lines = ser.readlines(5)   # read multiple '\n' terminated lines (timeout)
 	
 	print(x)
 	f.write(x)
@@ -15,6 +15,6 @@ with serial.Serial('/dev/ttyUSB0', 9600, timeout=1) as ser:
 	f.write(s)
 	print(line)
 	f.write(line)
-	print(lines)
-	f.write(lines)
+	#print(lines)
+	#f.write(lines)
 	f.close()
