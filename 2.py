31e328bc8bec3141f38554c448bed65ede87d0fc
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-27 23:25:01 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -7,4 +7,6 @@ with serial.Serial('/dev/ttyUSB0', 19200, timeout=1) as ser:
 	x = ser.read()          # read one byte
 	s = ser.read(10)        # read up to ten bytes (timeout)
 	line = ser.readline()   # read a '\n' terminated line
+	print(x)
 	print(s)
+	print(line)
