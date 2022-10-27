62bb43015465894dc2fed3143b2cfbf8b1a36b7b
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-27 23:27:14 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@ import serial
 
 
 
-with serial.Serial('/dev/ttyUSB0', 19200, timeout=1) as ser:
+with serial.Serial('/dev/ttyUSB0', 9600, timeout=10) as ser:
 	print(ser.name)         # check which port was really used
 	x = ser.read()          # read one byte
 	s = ser.read(10)        # read up to ten bytes (timeout)
