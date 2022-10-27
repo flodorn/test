eb5fae4125c12282515bec588b66f9b96c4eec6a
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-27 23:19:15 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,14 +6,11 @@ ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 print(ser.name)         # check which port was really used
 
 
-
-
 instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)  # port name, slave address (in decimal)
 
 ## Read temperature (PV = ProcessValue) ##
 try:
-    
-    temperature = instrument.read_register(289, 1)  # Registernumber, number of decimals
+	temperature = instrument.read_register(289, 1)  # Registernumber, number of decimals
 	print(temperature)
 except IOError:
     print("Failed to read from instrument")
@@ -26,6 +23,5 @@ try:
 	instrument.write_register(24, NEW_TEMPERATURE, 1)  # Registernumber, value, number of decimals for storage
 except IOError:
 	print("failed to write to register")
-
 
 ser.close()             # close port
