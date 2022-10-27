431edbab27632721150a86c6b5acee8efda68a11
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-27 23:48:29 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,5 @@
 import serial
-
-
+f = open("log.txt", "a")
 
 with serial.Serial('/dev/ttyUSB0', 9600, timeout=1) as ser:
 	print(ser.name)         # check which port was really used
@@ -9,6 +8,12 @@ with serial.Serial('/dev/ttyUSB0', 9600, timeout=1) as ser:
 	line = ser.readline()   # read a '\n' terminated line
 	lines = ser.readlines(5)   # read multiple '\n' terminated lines (timeout)
 	print(x)
+	f.write(x)
 	print(s)
+	f.write(s)
 	print(line)
+	f.write(line)
 	print(lines)
+	f.write(lines)
+	
+f.close()
