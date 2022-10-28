c19c88f539999a438a14b558f101c0632c89f069
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-28 22:41:27 +0200

    Update client_async2.py

--- client_async2.py
+++ client_async2.py
@@ -2,10 +2,12 @@ from pymodbus.client import AsyncModbusSerialClient
 
 async def run():
     client = AsyncModbusSerialClient("/dev/ttyUSB0")
-
+    
+    
+	
     await client.connect()
-    rr = await async_client.read_coils(0x01)
-    await async_client.write_coil(0x01, values)
+    rr = await async_client.read_coils(0x03)
+    await async_client.write_coil(0x03, rr)
     print(values)
     await client.close()
 
