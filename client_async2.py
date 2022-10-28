4a05866dce3941ee713101b4c069ee7529ad964e
flodorn <44878709+flodorn@users.noreply.github.com>
2022-10-28 22:35:07 +0200

    Update client_async2.py

--- client_async2.py
+++ client_async2.py
@@ -4,5 +4,9 @@ async def run():
     client = AsyncModbusSerialClient("/dev/ttyUSB0")
 
     await client.connect()
-    ...
+    rr = await async_client.read_coils(0x01)
+    await async_client.write_coil(0x01, values)
+    print(values)
     await client.close()
+
+    
